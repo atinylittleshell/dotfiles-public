004e7a293debb2051dc511e846a33e7ed7e64ce5
Kun Chen <kunchenguid@gmail.com>
2022-08-28 13:55:15 -0700

    luasnip

--- nvim/config/cmp.lua
+++ nvim/config/cmp.lua
@@ -1,8 +1,15 @@
 return {
-  "hrsh7th/nvim-cmp",
+  'hrsh7th/nvim-cmp',
   config = function()
-    local cmp = require("cmp")
+    local cmp = require('cmp')
+    if (cmp == nil) then return end
+
     cmp.setup({
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end
+      },
       mapping = cmp.mapping.preset.insert({
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
@@ -13,6 +20,7 @@ return {
       }),
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
+        { name = 'luasnip' },
         { name = 'buffer' },
       })
     })
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,66 +1,68 @@
-package.path = package.path .. ";" .. os.getenv("DOT_FILES_HOME") .. "/nvim/?.lua"
+package.path = package.path .. ';' .. os.getenv('DOT_FILES_HOME') .. '/nvim/?.lua'
 
-local packer_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local packer_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
-  vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. packer_path)
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. packer_path)
 end
 
-local packer_group = vim.api.nvim_create_augroup("Packer", { clear = true })
+local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
 vim.api.nvim_create_autocmd(
-  "BufWritePost",
-  { command = "source <afile> | PackerCompile", group = packer_group, pattern = "init.lua" }
+  'BufWritePost',
+  { command = 'source <afile> | PackerCompile', group = packer_group, pattern = 'init.lua' }
 )
 
-require("packer").startup(function(use)
+require('packer').startup(function(use)
   -- common libraries
-  use "wbthomason/packer.nvim"
-  use "nvim-lua/plenary.nvim"
-  use(require("config.mason"))
+  use 'wbthomason/packer.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use(require('config.mason'))
 
   -- ui
-  use(require("config.bufferline"))
-  use(require("config.lualine"))
-  use(require("config.rose-pine"))
+  use(require('config.bufferline'))
+  use(require('config.lualine'))
+  use(require('config.rose-pine'))
   use {
-    "kyazdani42/nvim-web-devicons",
-    config = function() require("nvim-web-devicons").setup() end
+    'kyazdani42/nvim-web-devicons',
+    config = function() require('nvim-web-devicons').setup() end
   }
-  use(require("config.markdown-preview"))
-  use(require("config.telescope"))
-  use(require("config.telescope-file-browser"))
-  use(require("config.fterm"))
+  use(require('config.markdown-preview'))
+  use(require('config.telescope'))
+  use(require('config.telescope-file-browser'))
+  use(require('config.fterm'))
 
   -- lsp
-  use "williamboman/mason-lspconfig.nvim"
-  use(require("config.lspconfig"))
-  use(require("config.lspsaga"))
-  use "hrsh7th/cmp-buffer"
-  use "hrsh7th/cmp-nvim-lsp"
-  use(require("config.cmp"))
-  use(require("config.null-ls"))
-  use(require("config.lsp-lines"))
+  use 'williamboman/mason-lspconfig.nvim'
+  use(require('config.lspconfig'))
+  use(require('config.lspsaga'))
+  use(require('config.luasnip'))
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'saadparwaiz1/cmp_luasnip'
+  use(require('config.cmp'))
+  use(require('config.null-ls'))
+  use(require('config.lsp-lines'))
 
   -- editing
-  use(require("config.treesitter"))
-  use "nvim-treesitter/nvim-treesitter-context"
+  use(require('config.treesitter'))
+  use 'nvim-treesitter/nvim-treesitter-context'
   use {
-    "windwp/nvim-autopairs",
+    'windwp/nvim-autopairs',
     config = function()
-      require("nvim-autopairs").setup()
+      require('nvim-autopairs').setup()
     end
   }
   use {
-    "windwp/nvim-ts-autotag",
+    'windwp/nvim-ts-autotag',
     config = function()
-      require("nvim-ts-autotag").setup()
+      require('nvim-ts-autotag').setup()
     end
   }
-  use "editorconfig/editorconfig-vim"
+  use 'editorconfig/editorconfig-vim'
 end)
 
 -- options
-vim.o.clipboard = "unnamedplus"
+vim.o.clipboard = 'unnamedplus'
 vim.o.number = true
 vim.o.title = true
 vim.o.ignorecase = true
@@ -69,6 +71,6 @@ vim.o.splitright = true
 vim.o.shiftwidth = 2
 vim.o.tabstop = 2
 vim.o.si = true
-vim.o.signcolumn = "yes"
+vim.o.signcolumn = 'yes'
 
-require("config.keymap")
+require('config.keymap')
