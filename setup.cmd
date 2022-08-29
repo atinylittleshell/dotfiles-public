056089cb4d24c41269d06f5ec55d9bfaac37dcf2
Kun Chen <kunchenguid@users.noreply.github.com>
2022-08-29 00:44:12 -0700

    Update setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -1,4 +1,2 @@
-curl.exe --create-dirs --output %LOCALAPPDATA%\nvim-data\site\autoload\plug.vim --url https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 SET script_root=%~dp0
 mklink /d %LOCALAPPDATA%\nvim %script_root%nvim
