75302b88e1426534e77495cfb8d8dd40f7ce1fd1
Kun Chen <kunchenguid@gmail.com>
2022-09-01 12:13:30 -0700

    lvim setup

--- profile.ps1
+++ profile.ps1
@@ -3,3 +3,6 @@ Set-PoshPrompt -Theme emodipt
 
 Set-PSReadLineKeyHandler -Chord Ctrl+f -Function ForwardChar
 Set-PSReadLineKeyHandler -Chord Ctrl+u -Function RevertLine
+
+$lvim_bin = "$HOME\.local\bin\lvim.ps1"
+Set-Alias lvim $lvim_bin
--- setup.ps1
+++ setup.ps1
@@ -9,22 +9,22 @@ if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administ
   return
 }
 
-if (Test-Path $profile -and ((Get-Item $profile).LinkType -ne "SymbolicLink")) {
+if ((Test-Path $profile) -and ((Get-Item $profile).LinkType -ne "SymbolicLink")) {
   Write-Warning "Deleting existing profile: $profile"
   Remote-Item -Force $profile
 }
 New-Item -Force -ItemType SymbolicLink -Path $profile -Target (Join-Path -Path $PSScriptRoot -ChildPath "profile.ps1")
 
 $nvimLink = Join-Path -Path $env:LOCALAPPDATA -ChildPath "nvim"
-if (Test-Path $nvimLink -and ((Get-Item $nvimLink).LinkType -ne "SymbolicLink")) {
-  Write-Warning "Deleting existing nvim link: $nvimLink"
+if ((Test-Path $nvimLink) -and ((Get-Item $nvimLink).LinkType -ne "SymbolicLink")) {
+  Write-Warning "Deleting existing nvim directory: $nvimLink"
   Remove-Item -Force -Recurse $nvimLink
 }
 New-Item -Force -ItemType SymbolicLink -Path $nvimLink -Target (Join-Path -Path $PSScriptRoot -ChildPath "nvim")
 
 $lvimLink = Join-Path -Path $env:LOCALAPPDATA -ChildPath "lvim"
-if (Test-Path $lvimLink -and ((Get-Item $lvimLink).LinkType -ne "SymbolicLink")) {
-  Write-Warning "Deleting existing lvim link: $lvimLink"
+if ((Test-Path $lvimLink) -and ((Get-Item $lvimLink).LinkType -ne "SymbolicLink")) {
+  Write-Warning "Deleting existing lvim directory: $lvimLink"
   Remove-Item -Force -Recurse $lvimLink
 }
 New-Item -Force -ItemType SymbolicLink -Path $lvimLink -Target (Join-Path -Path $PSScriptRoot -ChildPath "lvim")
