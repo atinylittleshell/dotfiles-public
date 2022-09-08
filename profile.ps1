ffca5a3cec815595c2751ce668e6fa3ebe43fe0e
Kun Chen <chenkun@microsoft.com>
2022-09-08 01:26:00 -0700

    update onmyposh config

--- profile.ps1
+++ profile.ps1
@@ -1,5 +1,6 @@
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/emodipt.omp.json" | Invoke-Expression
+
 $env:AZ_ENABLED=$false
-Set-PoshPrompt -Theme emodipt
 
 Set-PSReadLineKeyHandler -Chord Ctrl+f -Function ForwardChar
 Set-PSReadLineKeyHandler -Chord Ctrl+u -Function RevertLine
