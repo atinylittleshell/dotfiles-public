9842624f41a5288551b6868614d8ab1d7f421ac8
Kun Chen <kunchenguid@users.noreply.github.com>
2022-09-08 00:38:11 -0700

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,8 +10,8 @@ return {
   font_size = 12.0,
   color_scheme = 'Rosé Pine Moon (base16)',
   tab_max_width = 64,
+  tab_bar_at_bottom = true,
   use_fancy_tab_bar = false,
-  window_decorations = "RESIZE",
   window_frame = {
     font_size = 12.0,
   },
