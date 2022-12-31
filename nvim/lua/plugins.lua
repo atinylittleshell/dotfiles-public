b5c818309b688cb517452f92c8bd30fa2b3a3489
Kun Chen <kunchenguid@gmail.com>
2022-12-31 13:57:45 -0800

    tweak config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -115,6 +115,23 @@ require('lazy').setup({
   {
     'akinsho/toggleterm.nvim',
     lazy = false,
+    init = function()
+      -- powershell on windows
+      if vim.loop.os_uname().sysname == 'Windows_NT' then
+        local powershell_options = {
+          shell = vim.fn.executable('pwsh') and 'pwsh' or 'powershell',
+          shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
+          shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait',
+          shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
+          shellquote = '',
+          shellxquote = '',
+        }
+
+        for option, value in pairs(powershell_options) do
+          vim.opt[option] = value
+        end
+      end
+    end,
     config = function()
       require('plugins_config.toggleterm')
     end,
@@ -123,7 +140,8 @@ require('lazy').setup({
   -- autocompletion
   {
     'github/copilot.vim',
-    event = 'InsertEnter',
+    lazy = false,
+    priority = 51,
   },
   {
     'editorconfig/editorconfig-vim',
--- nvim/lua/vim_config.lua
+++ nvim/lua/vim_config.lua
@@ -66,19 +66,3 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     vim.lsp.buf.format({ timeout_ms = 10000 })
   end,
 })
-
--- powershell on windows
-if vim.loop.os_uname().sysname == 'Windows_NT' then
-  local powershell_options = {
-    shell = vim.fn.executable('pwsh') and 'pwsh' or 'powershell',
-    shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
-    shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait',
-    shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
-    shellquote = '',
-    shellxquote = '',
-  }
-
-  for option, value in pairs(powershell_options) do
-    vim.opt[option] = value
-  end
-end
