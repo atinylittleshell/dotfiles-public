ab74a5a4e8da8cd5ebb8dc7919d1a6ee841e74d6
Kun Chen <kunchenguid@gmail.com>
2023-04-08 22:41:25 -0700

    fix clipboard in wsl

--- nvim/lua/vim_config.lua
+++ nvim/lua/vim_config.lua
@@ -78,3 +78,19 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 vim.diagnostic.config({
   virtual_text = false,
 })
+
+-- WSL clipboard
+if os.getenv('WSL_DISTRO_NAME') then
+  g.clipboard = {
+    name = 'WslClipboard',
+    copy = {
+      ['+'] = 'clip.exe',
+      ['*'] = 'clip.exe',
+    },
+    paste = {
+      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
