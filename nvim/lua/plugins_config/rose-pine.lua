ecff1dfa384b96e292211a6dc0f098d4bb8f0e7b
Kun Chen <kunchenguid@gmail.com>
2023-04-22 16:16:04 -0700

    highlighting tweak

--- nvim/lua/plugins_config/rose-pine.lua
+++ nvim/lua/plugins_config/rose-pine.lua
@@ -2,4 +2,7 @@ require('rose-pine').setup({
   disable_italics = true,
   dark_variant = 'moon',
 })
+
 vim.cmd('colorscheme rose-pine')
+
+vim.api.nvim_set_hl(0, '@type.qualifier.typescript', { link = 'Keyword' })
