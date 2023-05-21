ca50c3f45e70f97404993f40e90703fd5c3d9ece
Kun Chen <kunchenguid@gmail.com>
2023-05-21 15:31:57 -0700

    disable clipboard in wsl

--- nvim/lua/vim_config.lua
+++ nvim/lua/vim_config.lua
@@ -81,16 +81,5 @@ vim.diagnostic.config({
 
 -- WSL clipboard
 if os.getenv('WSL_DISTRO_NAME') then
-  g.clipboard = {
-    name = 'WslClipboard',
-    copy = {
-      ['+'] = 'clip.exe',
-      ['*'] = 'clip.exe',
-    },
-    paste = {
-      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    },
-    cache_enabled = 0,
-  }
+  o.clipboard = ''
 end
