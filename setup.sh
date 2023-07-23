db0a1d764ba73557f830eaa8a1025b146e2f19d5
Kun Chen <kunchenguid@gmail.com>
2023-07-23 15:52:47 -0700

    improve linux script

--- setup.sh
+++ setup.sh
@@ -1,31 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-if [ "$(uname)" == "Darwin" ]; then
-  brew tap homebrew/cask-versions
-	brew install git
-	brew install zsh
-  brew install ripgrep
-  brew install neovim
-  mkdir -p ~/.zsh
-  git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
-	brew install starship
-else
-	sudo apt update
-	sudo apt install git
-	sudo apt install zsh
-  sudo apt install fd-find
-  sudo apt-get install ripgrep zip unzip
-  mkdir -p ~/.zsh
-  git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
-	curl -sS https://starship.rs/install.sh | sh
-
-  sudo rm /usr/bin/nvim
-  sudo ln -s $SCRIPT_DIR/nvim.appimage /usr/bin/nvim
-fi
-
 rm ~/.zshrc
 ln -s $SCRIPT_DIR/.zshrc ~/.zshrc
 
@@ -35,3 +10,26 @@ ln -s $SCRIPT_DIR/starship.toml ~/.config/starship.toml
 
 rm ~/.config/nvim
 ln -s $SCRIPT_DIR/nvim ~/.config/nvim
+
+if [ "$(uname)" == "Darwin" ]; then
+  brew tap homebrew/cask-versions
+  brew install git
+  brew install zsh
+  brew install ripgrep
+  brew install neovim
+  mkdir -p ~/.zsh
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+  brew install starship
+else
+  sudo apt update
+  sudo apt install git curl zsh fd-find ripgrep zip unzip
+  mkdir -p ~/.zsh
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+
+  sudo rm /usr/bin/nvim
+  sudo ln -s $SCRIPT_DIR/nvim.appimage /usr/bin/nvim
+
+  curl -sS https://starship.rs/install.sh | sh
+fi
