5313cf2e2c61408a1fbcab0814d6b3afbe6a3986
Kun Chen <kunchenguid@gmail.com>
2024-02-03 11:08:31 -0800

    remove snippets

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,30 +1,10 @@
 return {
   {
-    'L3MON4D3/LuaSnip',
-    version = '*',
-    dependencies = {
-      { 'rafamadriz/friendly-snippets' },
-    },
-    config = function()
-      local luasnip = require('luasnip')
-
-      luasnip.config.set_config({
-        history = true,
-        updateevents = 'TextChanged,TextChangedI',
-        enable_autosnippets = true,
-      })
-
-      require('luasnip.loaders.from_vscode').lazy_load()
-    end,
-  },
-  {
     'hrsh7th/nvim-cmp',
     version = '*',
     event = 'InsertEnter',
     dependencies = {
-      'L3MON4D3/LuaSnip',
       { 'hrsh7th/cmp-nvim-lsp' },
-      { 'saadparwaiz1/cmp_luasnip' },
       { 'hrsh7th/cmp-buffer' },
       { 'hrsh7th/cmp-nvim-lua' },
       { 'hrsh7th/cmp-path' },
@@ -43,11 +23,6 @@ return {
             return not context.in_treesitter_capture('comment') and not context.in_syntax_group('Comment')
           end
         end,
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
-        },
         mapping = cmp.mapping.preset.insert({
           ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
           ['<C-u>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
@@ -63,13 +38,11 @@ return {
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
           { name = 'nvim_lua' },
-          { name = 'luasnip' },
           { name = 'buffer' },
           { name = 'path' },
         }),
         source_priority = {
           nvim_lsp = 1000,
-          luasnip = 750,
           buffer = 500,
           path = 250,
         },
