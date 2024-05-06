96831ddc2cdb8eaaf4b68519b61e71babc3fb46d
atinylittleshell <shell@atinylittleshell.me>
2024-05-05 22:52:31 -0700

    install terminal one on nixos

--- home-manager/pkgs/terminal-one.nix
+++ home-manager/pkgs/terminal-one.nix
@@ -1,20 +1,21 @@
-{ stdenv, fetchurl, lib }:
+{ stdenv, fetchurl, lib, appimageTools }:
 
-stdenv.mkDerivation rec {
+let
   pname = "terminal-one";
   version = "1.6.3";
+  name = "${pname}-${version}";
 
   src = fetchurl {
     url = "https://github.com/atinylittleshell/TerminalOne/releases/download/v${version}/TerminalOne-${version}-x86_64.AppImage";
     sha256 = "sha256-uJ0IEPWjy0W6Uz55UAKJDhdYCTuU0T1Yr/YFFyv5hQ4=";
   };
 
-  dontUnpack = true;
+  appimageContents = appimageTools.extractType2 { inherit name src; };
+in appimageTools.wrapType2 rec {
+  inherit name src;
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp $src $out/bin/${pname}
-    chmod +x $out/bin/${pname}
+  extraInstallCommands = ''
+    mv $out/bin/${name} $out/bin/${pname}
   '';
 
   meta = with lib; {
