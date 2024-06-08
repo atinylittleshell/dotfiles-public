87231dd9e7d08e0cac6b5e6ce7501ff3c5cb5c5f
atinylittleshell <shell@atinylittleshell.me>
2024-06-08 00:59:59 -0700

    fix picom

--- home-manager/files/.config/awesome/main/startup.lua
+++ home-manager/files/.config/awesome/main/startup.lua
@@ -1,2 +1,5 @@
-os.execute("xset r rate 200 30")
-os.execute("xrandr --output DP-4 --mode 5120x1440")
+local awful = require("awful")
+
+awful.spawn.with_shell("xset r rate 200 30")
+awful.spawn.with_shell("xrandr --output DP-4 --mode 5120x1440")
+awful.spawn.with_shell("picom -b")
