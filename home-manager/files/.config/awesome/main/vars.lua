c03d5f048759b6a0b0410fe64576ade90bd82558
atinylittleshell <shell@atinylittleshell.me>
2024-07-08 00:38:50 -0700

    use alt as modkey for awesomewm

--- home-manager/files/.config/awesome/main/vars.lua
+++ home-manager/files/.config/awesome/main/vars.lua
@@ -9,6 +9,6 @@ M.editor_cmd = M.terminal .. " -e " .. M.editor
 -- If you do not like this or do not have such a key,
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
-M.modkey = "Mod4"
+M.modkey = "Mod1"
 
 return M
