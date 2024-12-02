0c3891f566e5c094f7e9f86312ec3fcf8232973f
atinylittleshell <shell@atinylittleshell.me>
2024-12-02 00:36:28 -0800

    wezterm config

--- home-manager/files/.config/wezterm/wezterm.lua
+++ home-manager/files/.config/wezterm/wezterm.lua
@@ -1,16 +1,24 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
 
 -- ui
 config.color_scheme = "rose-pine-moon"
 config.max_fps = 120
 config.win32_system_backdrop = "Acrylic"
 config.window_background_opacity = 0.7
+config.font = wezterm.font("Hack Nerd Font", { weight = "DemiBold" })
+
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.window_frame = {
+  font = wezterm.font("Hack Nerd Font", { weight = "Bold" }),
+  font_size = 10.0,
+}
+
+config.inactive_pane_hsb = {
+  saturation = 0.0,
+  brightness = 0.5,
+}
 
 -- shell
 local is_windows = os.getenv("OS") and os.getenv("OS"):lower():find("windows")
@@ -19,5 +27,73 @@ if is_windows then
   config.default_domain = "WSL:Ubuntu-24.04"
 end
 
--- and finally, return the configuration to wezterm
+-- keys
+config.disable_default_key_bindings = true
+config.leader = { key = "b", mods = "CTRL" }
+config.keys = {
+  {
+    key = "c",
+    mods = "LEADER",
+    action = wezterm.action.ShowLauncher,
+  },
+  {
+    key = "&",
+    mods = "LEADER|SHIFT",
+    action = wezterm.action.CloseCurrentTab({ confirm = false }),
+  },
+  {
+    key = "n",
+    mods = "LEADER",
+    action = wezterm.action.ActivateTabRelative(1),
+  },
+  {
+    key = "p",
+    mods = "LEADER",
+    action = wezterm.action.ActivateTabRelative(-1),
+  },
+  {
+    key = '"',
+    mods = "LEADER|SHIFT",
+    action = wezterm.action.SplitHorizontal,
+  },
+  {
+    key = "%",
+    mods = "LEADER|SHIFT",
+    action = wezterm.action.SplitVertical,
+  },
+  {
+    key = "h",
+    mods = "LEADER",
+    action = wezterm.action.ActivatePaneDirection("Left"),
+  },
+  {
+    key = "l",
+    mods = "LEADER",
+    action = wezterm.action.ActivatePaneDirection("Right"),
+  },
+  {
+    key = "k",
+    mods = "LEADER",
+    action = wezterm.action.ActivatePaneDirection("Up"),
+  },
+  {
+    key = "j",
+    mods = "LEADER",
+    action = wezterm.action.ActivatePaneDirection("Down"),
+  },
+  {
+    key = "x",
+    mods = "LEADER",
+    action = wezterm.action.CloseCurrentPane({ confirm = false }),
+  },
+}
+
+for i = 1, 8 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "LEADER",
+    action = wezterm.action.ActivateTab(i - 1),
+  })
+end
+
 return config
