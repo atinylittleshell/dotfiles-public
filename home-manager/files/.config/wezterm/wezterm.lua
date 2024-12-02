5cdee47c3488af54fd2cb947d90f3be0fccdd3af
atinylittleshell <shell@atinylittleshell.me>
2024-12-01 22:01:33 -0800

    wsl support in wezterm

--- home-manager/files/.config/wezterm/wezterm.lua
+++ home-manager/files/.config/wezterm/wezterm.lua
@@ -6,12 +6,18 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
+-- ui
 config.color_scheme = "rose-pine-moon"
-
 config.max_fps = 120
 config.win32_system_backdrop = "Acrylic"
 config.window_background_opacity = 0.7
+
+-- shell
+local is_windows = os.getenv("OS") and os.getenv("OS"):lower():find("windows")
+
+if is_windows then
+  config.default_domain = "WSL:Ubuntu-24.04"
+end
 
 -- and finally, return the configuration to wezterm
 return config
