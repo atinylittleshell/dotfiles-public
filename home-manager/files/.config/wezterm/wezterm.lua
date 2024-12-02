3987d8243f073651b621a85967e0536da4fa74c5
Kun Chen <kchen6@atlassian.com>
2024-12-02 10:38:21 -0800

    wezterm config on mac

--- home-manager/files/.config/wezterm/wezterm.lua
+++ home-manager/files/.config/wezterm/wezterm.lua
@@ -2,17 +2,17 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+local is_windows = os.getenv("OS") and os.getenv("OS"):lower():find("windows")
+local is_macos = wezterm.target_triple:lower():find("darwin") ~= nil
+
 -- ui
 config.color_scheme = "rose-pine-moon"
 config.max_fps = 120
-config.win32_system_backdrop = "Acrylic"
-config.window_background_opacity = 0.7
 config.font = wezterm.font("Hack Nerd Font", { weight = "DemiBold" })
 
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.window_frame = {
   font = wezterm.font("Hack Nerd Font", { weight = "Bold" }),
-  font_size = 10.0,
 }
 
 config.inactive_pane_hsb = {
@@ -20,9 +20,20 @@ config.inactive_pane_hsb = {
   brightness = 0.5,
 }
 
--- shell
-local is_windows = os.getenv("OS") and os.getenv("OS"):lower():find("windows")
+if is_windows then
+  config.win32_system_backdrop = "Acrylic"
+  config.window_background_opacity = 0.7
+  config.window_frame.font_size = 10.0
+end
 
+if is_macos then
+  config.window_background_opacity = 0.5
+  config.macos_window_background_blur = 100
+  config.font_size = 15.0
+  config.window_frame.font_size = 13.0
+end
+
+-- shell
 if is_windows then
   config.default_domain = "WSL:Ubuntu-24.04"
 end
